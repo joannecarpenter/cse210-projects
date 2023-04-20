cb6c1f82532854d830fd7d895cbbfb511d0a8b2a
Joanne Carpenter <car22047@byui.edu>
2023-04-20 12:02:13 -0400

    Completed Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Random number is generated for the magic number.
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        
+        // User enters the magic number.
+        // Console.Write("What is the magic number? ");
+        // string userNumber = Console.ReadLine();
+        // int number = int.Parse(userNumber);
+        
+        int guess = 9999;
+        int count = 0;
+        
+        while (guess != number)
+        {
+             // User tries to guess the magic number.
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            guess = int.Parse(userGuess);
+            count += 1;
+
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+
+        }
+
+        if (guess == number)
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It only took you {count} guesses!");
+            }
     }
 }
